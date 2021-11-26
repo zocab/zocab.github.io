80227e486a08858a2223593b3729ec7ddc1c07bd
zocab <94276310+zocab@users.noreply.github.com>
2021-11-26 08:37:55 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,200 @@
+const DATA = [
+  {
+    "_id": "1622464446965_philosophy_642854d159e36",
+    "word": "philosophy",
+    "meaning": "triết học",
+    "pronounce": "/fɪˈlɒsəfi/",
+    "type": "(n.)",
+    "ex_en": "She studied politics and philosophy.",
+    "ex_vi": "Cô ấy đã học chính trị và triết học.",
+    "image": "philosophy2.jpg",
+    "audio": "philosophy.mp3",
+  },
+  {
+    "_id": "1622464447196_praise_6d8212a384527",
+    "word": "praise",
+    "meaning": "khen ngợi, tuyên dương",
+    "pronounce": "/preɪz/",
+    "type": "(v.)",
+    "ex_en": "She praised his cooking.",
+    "ex_vi": "Cô ấy khen ngợi việc nấu ăn của anh ấy.",
+    "image": "praise2.jpg",
+    "audio": "praise.mp3",
+  },
+  {
+    "_id": "1622464447385_retire_ab513745f6ebf",
+    "word": "retire",
+    "meaning": "nghỉ hưu",
+    "pronounce": "/ri'taiə/",
+    "type": "(v.)",
+    "ex_en": "She was forced to retire early from teaching because of ill health",
+    "ex_vi": "Cô bị buộc phải nghỉ hưu sớm trong việc giảng dạy vì lý do sức khỏe",
+    "image": "retire1.png",
+    "audio": "retire.mp3",
+  },
+  {
+    "_id": "1622464447600_retired_2319d5cb9526d",
+    "word": "retired",
+    "meaning": "nghỉ hưu",
+    "pronounce": "/rɪˈtaɪəd/",
+    "type": "(adj.)",
+    "ex_en": "Dad is retired now.",
+    "ex_vi": "Hiện nay, cha tôi đã nghỉ hưu.",
+    "image": "retired2.jpg",
+    "audio": "retired.mp3",
+  },
+  {
+    "_id": "1622464447811_scare_90aa9231c4692",
+    "word": "scare",
+    "meaning": "làm kinh hãi, làm sợ hãi, doạ",
+    "pronounce": "/skeə(r)/",
+    "type": "(v.)",
+    "ex_en": "Your masks scare me.",
+    "ex_vi": "Mặt nạ của cậu làm tớ sợ đấy.",
+    "image": "scare1.png",
+    "audio": "scare.mp3",
+  },
+  {
+    "_id": "1622464448015_sincerely_93192a4154688",
+    "word": "sincerely",
+    "meaning": "một cách chân thành",
+    "pronounce": "/sɪnˈsɪəli/",
+    "type": "(adv.)",
+    "ex_en": "I sincerely believe that this is the right decision.",
+    "ex_vi": "Tôi chân thành tin rằng đây là quyết định đúng.",
+    "image": "i_sincerel_ex2_587727f37f8b9a0d308b460e.jpg",
+    "audio": "sincerely.mp3",
+  },
+  {
+    "_id": "1622464448234_six_bafb3f72910df",
+    "word": "six",
+    "meaning": "số sáu",
+    "pronounce": "/sɪks/",
+    "type": "(cardinal no.)",
+    "ex_en": "She's lived here six months.",
+    "ex_vi": "Cô ấy đã sống ở đây sáu tháng.",
+    "image": "six2.png",
+    "audio": "six.mp3",
+  },
+  {
+    "_id": "1622464448412_skilled_fb40afa5455c9",
+    "word": "skilled",
+    "meaning": "lành nghề; có khả năng, kinh nghiệm và kiến thức làm gì",
+    "pronounce": "/skɪld/",
+    "type": "(adj.)",
+    "ex_en": "He is a skilled carpenter.",
+    "ex_vi": "Anh ấy là một thợ mộc lành nghề.",
+    "image": "skilled2.png",
+    "audio": "skilled.mp3",
+  },
+  {
+    "_id": "1622464448603_sleep_611f00414960e",
+    "word": "sleep",
+    "meaning": "sự, giấc ngủ",
+    "pronounce": "/sliːp/",
+    "type": "(n.)",
+    "ex_en": "His talk nearly sent me to sleep(= it was boring).",
+    "ex_vi": "Cuộc nói chuyện của anh ta gần như đã đưa tôi vào giấc ngủ (= Nó nhàm chán).",
+    "image": "sleep2.jpg",
+    "audio": "sleep.mp3",
+  },
+  {
+    "_id": "1622464448771_status_9e9128099d95b",
+    "word": "status",
+    "meaning": "địa vị (về mặt pháp lí)",
+    "pronounce": "/ˈsteɪtəs/",
+    "type": "(n.)",
+    "ex_en": "The party was denied legal status.",
+    "ex_vi": "Các bên đã bị từ chối tình trạng pháp lý.",
+    "image": "status2.jpg",
+    "audio": "status.mp3",
+  },
+  {
+    "_id": "1622464449201_striped_f78e6210006b7",
+    "word": "striped",
+    "meaning": "có sọc, có vằn",
+    "pronounce": "/straɪpt/",
+    "type": "(adj.)",
+    "ex_en": "My home usually has a white table - cloth with red striped.",
+    "ex_vi": "Nhà tôi thường dùng khăn trải bàn trắng có sọc đỏ.",
+    "image": "my_home_us_ex2_587727f47f8b9a0d308b4614.jpg",
+    "audio": "striped.mp3",
+  },
+  {
+    "_id": "1622464449423_this_d4e7a77bf1b3a",
+    "word": "this",
+    "meaning": "đây, này",
+    "pronounce": "/ðɪs/",
+    "type": "(det.)",
+    "ex_en": "This is my teacher.",
+    "ex_vi": "Đây là giáo viên của tôi.",
+    "image": "this1.jpg",
+    "audio": "this.mp3",
+  },
+  {
+    "_id": "1622464449597_tidy_51bab6e5a6c41",
+    "word": "tidy",
+    "meaning": "dọn dẹp",
+    "pronounce": "/ˈtaɪdi/",
+    "type": "(v.)",
+    "ex_en": "I spent all morning cleaning and tidying.",
+    "ex_vi": "Tôi đã dành cả buổi sáng làm sạch và dọn dẹp.",
+    "image": "tidy2.jpg",
+    "audio": "tidy.mp3",
+  },
+  {
+    "_id": "1622464449771_aware_049c370382c84",
+    "word": "aware",
+    "meaning": "biết, nhận thức",
+    "pronounce": "/ə'weə/",
+    "type": "(adj.)",
+    "ex_en": "My dentist made me aware that I should have an appointment twice a year",
+    "ex_vi": "Nha sĩ của tôi làm cho tôi biết rằng tôi nên hẹn gặp 2 lần mỗi năm (để khám răng)",
+    "image": "my_dentist_ex1_56248d167f8b9a040dc66260.jpg",
+    "audio": "aware.mp3",
+  },
+  {
+    "_id": "1622464449947_city_01a9b6a91e3bb",
+    "word": "city",
+    "meaning": "thành phố",
+    "pronounce": "/ˈsɪti/",
+    "type": "(n.)",
+    "ex_en": "I live in Ho Chi Minh city.",
+    "ex_vi": "Tôi sống ở thành phố Hồ Chí Minh.",
+    "image": "city-l61.jpg",
+    "audio": "city-l6.mp3",
+  },
+]
+
+function randomNumber(min, max)  {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+window.addEventListener('load', function() {
+  const enExampleTarget = document.getElementById('enExample');
+  const viExampleTarget = document.getElementById('viExample');
+  const wordTarget = document.getElementById('word');
+  const pronunciation = document.getElementById('pronunciation');
+  
+  const index = randomNumber(0, DATA.length);
+  
+  wordTarget.innerText = DATA[index].word;
+  pronunciation.innerText = DATA[index].pronounce;
+  viExampleTarget.innerText = DATA[index].ex_vi;
+  enExampleTarget.innerText = DATA[index].ex_en;
+  
+  viExampleTarget.addEventListener('click', function() {
+    viExampleTarget.classList.toggle('blur');
+  })
+
+
+  let isEn = true;
+  wordTarget.addEventListener('click', function() {
+    if (isEn) {
+      wordTarget.innerHTML = DATA[index].meaning;
+    } else {
+      wordTarget.innerHTML = DATA[index].word;
+    }
+    isEn = !isEn;
+  })
+})
